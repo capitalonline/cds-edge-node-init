d1148845009d100d2d4dd1dd122f71d3f12aed66
jizhi.sui@capitalonline.net <jizhi.sui@capitalonline.net>
2020-10-14 14:32:18 +0800

    add in main

--- cmd/main.go
+++ cmd/main.go
@@ -20,10 +20,14 @@ func main () {
 	//err := pkg.SystemConfig()
 	//fmt.Println(err)
 
+	//if err := pkg.YumConfig(); err != nil {
+	//	fmt.Errorf("DockerInstall: failed, err is: %s\n", err.Error())
+	//}
+
 	if err := pkg.DockerInstall(); err != nil {
 		fmt.Errorf("DockerInstall: failed, err is: %s\n", err.Error())
 	}
-	
-	fmt.Printf("DockerInstall: Succeed! \n")
+
+	fmt.Printf("Finished init main\n")
 
 }
