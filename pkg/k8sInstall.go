79813781c42fea049c8528caaa70aebb6876385a
jizhi.sui@capitalonline.net <jizhi.sui@capitalonline.net>
2020-10-19 13:56:05 +0800

    fix bug 4

--- pkg/k8sInstall.go
+++ pkg/k8sInstall.go
@@ -11,42 +11,43 @@ func K8sInstall (k8sV17InitData *utils.K8sV17Config) error {
 	log.Infof("K8sInstall: starting")
 
 	// check
-	out, err:= utils.RunCommand("kubelet --version")
-	if err != nil && strings.Contains(err.Error(), "kubelet: command not found") {
-		// kubelet not installed, start do it
-		// wget kubernetes.repo
-		wgetCmd := fmt.Sprintf("wget -P /etc/yum.repos.d %s", k8sV17InitData.K8sInstall.RepoAdd)
-		if _, err := utils.RunCommand(wgetCmd); err != nil {
-			return err
-		}
-
-		// install kubeadm and kubelet and kubectl v1.17.0
-		for _, value := range k8sV17InitData.K8sInstall.Install {
-			installCmd := fmt.Sprintf("yum install -y %s", value)
-			if _, err := utils.RunCommand(installCmd); err != nil {
-				return err
-			}
-		}
-
-		// confirm
-		confirmCmd := fmt.Sprintf("kubelet --version")
-		out, err := utils.RunCommand(confirmCmd)
-		if err != nil {
-			return err
-		}
-		if !strings.Contains(out, k8sV17InitData.K8sInstall.Version) {
-			return fmt.Errorf("confirm kubelet install version %s failed", k8sV17InitData.K8sInstall.Version)
-		}
-
-	}
-
+	out, _:= utils.RunCommand("kubelet --version")
 	if strings.Contains(out, k8sV17InitData.K8sInstall.Version) {
-		log.Warnf("kubelet %s installed", k8sV17InitData.K8sInstall.Version)
+		log.Warnf("K8sInstall: kubelet %s installed, ignore install again!", k8sV17InitData.K8sInstall.Version)
 		return nil
-	} else {
-		log.Errorf("kubelet is installed, but out is: %s is different from expect version: %s", out, k8sV17InitData.K8sInstall.Version)
-		return fmt.Errorf("kubelet is installed, but out is: %s is different from expect version: %s", out, k8sV17InitData.K8sInstall.Version)
 	}
+
+	// wget kubernetes.repo
+	wgetCmd := fmt.Sprintf("wget -P /etc/yum.repos.d %s", k8sV17InitData.K8sInstall.RepoAdd)
+	if _, err := utils.RunCommand(wgetCmd); err != nil {
+		return err
+	}
+
+	// install kubeadm and kubelet and kubectl v1.17.0
+	for _, value := range k8sV17InitData.K8sInstall.Install {
+		installCmd := fmt.Sprintf("yum install -y %s", value)
+		if _, err := utils.RunCommand(installCmd); err != nil {
+			return err
+		}
+	}
+
+	// confirm
+	confirmCmd := fmt.Sprintf("kubelet --version")
+	out, err := utils.RunCommand(confirmCmd)
+	if err != nil {
+		return err
+	}
+	if !strings.Contains(out, k8sV17InitData.K8sInstall.Version) {
+		return fmt.Errorf("confirm kubelet installed version %s failed", k8sV17InitData.K8sInstall.Version)
+	}
+
+	//if strings.Contains(out, k8sV17InitData.K8sInstall.Version) {
+	//	log.Warnf("kubelet %s installed", k8sV17InitData.K8sInstall.Version)
+	//	return nil
+	//} else {
+	//	log.Errorf("kubelet is installed, but out is: %s is different from expect version: %s", out, k8sV17InitData.K8sInstall.Version)
+	//	return fmt.Errorf("kubelet is installed, but out is: %s is different from expect version: %s", out, k8sV17InitData.K8sInstall.Version)
+	//}
 
 	log.Infof("K8sInstall: Succeed!")
 	return nil
